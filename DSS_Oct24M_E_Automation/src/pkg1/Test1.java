03c09de27aae5b7f9c968fd80d4a256ea7d0a4eb
sandipdalavi1111 <sandipdalavi1111@gmail.com>
2025-01-25 19:48:37 +0530

    commit1

---
+++ DSS_Oct24M_E_Automation/src/pkg1/Test1.java
@@ -0,0 +1,35 @@
+package pkg1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+
+	public static WebDriver driver;
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeOptions co = new ChromeOptions();
+		
+        co.addArguments("--remote-allow-origins=*");
+        
+        driver=new ChromeDriver(co);
+        
+        driver.get("https://www.google.com/");
+        
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.tagName("a")).click();
+	}
+
+}
